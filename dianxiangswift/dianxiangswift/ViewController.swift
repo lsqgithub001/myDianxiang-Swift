f0a502c1b8fdd87b0f590ce17111c957c3540ed7
lsqgithub001 <225546147@qq.com>
2019-07-23 11:48:53 +0800

    创建工程

---
+++ dianxiangswift/dianxiangswift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dianxiangswift
+//
+//  Created by 灰常溜 on 2019/7/23.
+//  Copyright © 2019 灰常溜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ dianxiangswift/dianxiangswiftTests/dianxiangswiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  dianxiangswiftTests.swift
+//  dianxiangswiftTests
+//
+//  Created by 灰常溜 on 2019/7/23.
+//  Copyright © 2019 灰常溜. All rights reserved.
+//
+
+import XCTest
+@testable import dianxiangswift
+
+class dianxiangswiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
